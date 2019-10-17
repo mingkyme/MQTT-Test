10816a3dffd1ee98fdbd0db2e3029b1a51153ba9
mingky <admin@mingky.me>
2019-10-17 15:24:46 +0900

    TEST

---
+++ MQTT-Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+namespace MQTT_Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient client = new MqttClient("127.0.0.1");
+            client.Connect("mingkyme");
+            client.Publish("work/mingkyme",Encoding.UTF8.GetBytes("HTTPS://MINGKY.ME"));
+            
+        }
+
+        
+    }
+}
---
+++ MQTT_Subscriber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace MQTT_Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient client = new MqttClient("127.0.0.1");
+            client.Connect("mingkyme");
+            client.Subscribe(new string[] { "work/mingkyme" }, new byte[] { 2 });
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            client.MqttMsgSubscribed += Client_MqttMsgSubscribed;
+        }
+        private static void Client_MqttMsgSubscribed(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgSubscribedEventArgs e)
+        {
+            Console.WriteLine("Client_MqttMsgSubscribed");
+        }
+
+        private static void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(e.Message));
+
+        }
+    }
+}
